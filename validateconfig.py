9224a88a42d6e9d2cf1cd5e1a2d8c619ba571cd3
Alexander <ezendos@gmail.com>
2020-12-24 02:46:55 +0300

    Исправления

--- validateconfig.py
+++ validateconfig.py
@@ -19,38 +19,41 @@ import pathlib
 
 
 def vfile(args):  # file - проверка наличия имени файла
-    print('kek')
+    return True
 
 
 def vhash(args):  # hash
-    pass
+    return True
 
 
 def vuser(args):  # user
-    pass
+    return True
 
 
 def vgroup(args):  # group
-    pass
+    return True
 
 
 def errexit(entry):  # errexit - показать правильное использование и выйти
-    print(f"invalid syntax in entry\n{entry}")
+    print(f"invalid syntax in entry\n{entry.strip()}")
     print("usage: [!]file|hash|[!]user|[!]group [args]")
     sys.exit()
 
 
 def get_args(entry):
+    if entry[:1] == '#':  # комментарии пропускаются
+        return
+
     args = entry.split()
     if args:
         if args[0] == '!':
-            args[0] = False
+            args[0] = True
         elif '!' in args[0]:
             args[0] = args[0][1:]
             args[0].strip()
-            args.insert(0, False)
-        else:
             args.insert(0, True)
+        else:
+            args.insert(0, False)
 
         if args[1] in tasks.keys():  # проверка синтаксиса
             return args
@@ -65,17 +68,18 @@ tasks = {
     'group': vgroup
 }
 
-config_path = pathlib.Path('config.txt')  # default configuration file
+config_path = pathlib.Path('config.txt')  # конфигурационный файл по-умолчанию
 
 if len(sys.argv) > 1:
     config_path = pathlib.Path(sys.argv[1])
 
 
 with open(config_path) as f:
+    line = 0
     entries = f.readlines()
     for entry in entries:
-        if entry[:1] == '#':  # комментарии пропускаются
-            continue
+        line += 1
         entry_args = get_args(entry)
         if entry_args:
-            tasks[entry_args[1]](entry_args)
+            if tasks[entry_args[1]](entry_args):
+                print(f"Fault in line {line}: {entry.strip()}")
