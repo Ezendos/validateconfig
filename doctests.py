473c4b4d1dce7ade1b9cf30227baa094c544a19e
Nik <43304085+CryFR@users.noreply.github.com>
2020-12-26 14:55:28 +0300

    Update doctests.py

--- doctests.py
+++ doctests.py
@@ -2,16 +2,20 @@ import pathlib
 import doctest
 import hashlib
 import pwd
+import grp
+
 
 def vfile(args):  # file - проверка наличия имени файла
     """
-    >>> vfile([True, 'file', 'file']) #файла нет
+    файла file в системе - нет
+    файл config.txt - существует
+    >>> vfile([True, 'file', 'file']) #файла file не является файлом
     True
-    >>> vfile([False, 'file', 'file']) #файла нет
+    >>> vfile([False, 'file', 'file']) #файла file является файлом
     False
-    >>> vfile([True, 'file', 'config.txt']) #файл есть
+    >>> vfile([True, 'file', 'config.txt']) #файл config.txt не является файлом
     False
-    >>> vfile([False, 'file', 'config.txt']) #файл есть
+    >>> vfile([False, 'file', 'config.txt']) #файл config.txt является файлом
     True
     """
     fname = pathlib.Path(args[2])
@@ -29,13 +33,14 @@ def vfile(args):  # file - проверка наличия имени файла
 
 def vhash(args):  # hash
     """
-    >>> vhash([True, 'hash', '546e4328ddf422043a45c460e5f8fe3cd93db06a', 'abs.txt'])
+    hash для abs.txt == 546e4328ddf422043a45c460e5f8fe3cd93db06a
+    >>> vhash([True, 'hash', '546e4328ddf422043a45c460e5f8fe3cd93db06a', 'abs.txt']) #хэши совпадают
     True
-    >>> vhash([True, 'hash', '646e4328ddf422043a45c460e5f8fe3cd93db06a', 'abs.txt'])
+    >>> vhash([True, 'hash', '646e4328ddf422043a45c460e5f8fe3cd93db06a', 'abs.txt']) #хэши не совпадают
     False
-    >>> vhash([False, 'hash', '546e4328ddf422043a45c460e5f8fe3cd93db06a', 'abs.txt'])
+    >>> vhash([False, 'hash', '546e4328ddf422043a45c460e5f8fe3cd93db06a', 'abs.txt']) #инверсия для функции работать не должна
     True
-    >>> vhash([False, 'hash', '646e4328ddf422043a45c460e5f8fe3cd93db06a', 'abs.txt'])
+    >>> vhash([False, 'hash', '646e4328ddf422043a45c460e5f8fe3cd93db06a', 'abs.txt']) #инверсия для функции работать не должна
     False
     """
     a = hashlib.sha1()
@@ -49,6 +54,18 @@ def vhash(args):  # hash
 
 
 def vuser(args):  # user
+    """
+    user bono - не существует
+    user root - существует
+    >>> vuser([True, 'user', 'bono']) #user bono не существует
+    True
+    >>> vuser([False, 'user', 'bono']) #user bono существует
+    False
+    >>> vuser([True, 'user', 'root']) #user root не существует
+    False
+    >>> vuser([False, 'user', 'root']) #user root существует
+    True
+    """
     if args[0] == True:
         try:
             pwd.getpwnam(args[2])
@@ -62,5 +79,25 @@ def vuser(args):  # user
         except KeyError:
             return False
 
+
+def vgroup(args):
+    """
+    >>> vgroup([True, 'group', 'students']) #группы не существует
+    True
+    >>> vgroup([False, 'group', 'students']) #группа существует
+    False
+    """
+    if args[0] is True:
+        try:
+            grp.getgrnam(args[2])
+            return False
+        except KeyError:
+            return True
+    else:
+        try:
+            grp.getgrnam(args[2])
+            return True
+        except KeyError:
+            return False
 
 doctest.testmod()
